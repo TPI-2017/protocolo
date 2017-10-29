8e2a89102bab62611d7dc041758a5624ee372ab0
Ramiro <ramiroromero94@gmail.com>
2017-10-29 00:52:45 -0300

    Métodos vacíos

--- Message.cpp
+++ Message.cpp
@@ -1,17 +1,122 @@
 #include "Message.h"
 
-struct BaseMessage {
-	uint8_t version;
-	uint8_t type;
-	uint8_t size;
-	char payload[253];
+struct Text {
+	char text[252];
 }__attribute__((packed));
 
-struct SetTextRequest {
-	char text[253];
+struct WifiConfig {
+	char SSID[64];
+	char password[64];
+	uint32_t ip;
+	uint32_t subnetMask;
 }__attribute__((packed));
 
 struct ServerResponse {
 	uint8_t responseCode;
+	union {
+		WifiConfig wifiConfig;
+		Text text;
+	};
 }__attribute__((packed));
 
+struct BaseMessage {
+	uint8_t version;
+	uint8_t type;
+	uint8_t size;
+	union {
+		Text text;
+		WifiConfig wifiConfig;
+		ServerResponse response;
+	};
+}__attribute__((packed));
+
+Message::Message(const void *raw)
+{
+}
+
+Message::Message(Type type)
+: mType(type)
+{
+}
+
+void Message::prepare()
+{
+	#warning Not implemented.
+}
+
+// Getters
+const char *Message::text() const
+{
+	#warning Not implemented.
+}
+
+const char *Message::wifiSSID() const
+{
+	#warning Not implemented.
+}
+
+const char *Message::wifiPassword() const
+{
+	#warning Not implemented.
+}
+
+uint32_t Message::wifiIP() const
+{
+	#warning Not implemented.
+}
+
+uint32_t Message::wifiSubnet() const
+{
+	#warning Not implemented.
+}
+
+uint8_t Message::blinkRate() const
+{
+	#warning Not implemented.
+}
+
+uint8_t Message::slideRate() const
+{
+	#warning Not implemented.
+}
+
+const char *Message::password() const
+{
+	#warning Not implemented.
+}
+
+// Setters
+void setWifiSSID(const char *str)
+{
+	#warning Not implemented.
+}
+
+void setWifiPassword(const char *password)
+{
+	#warning Not implemented.
+}
+
+void setWifiIP(uint32_t ip)
+{
+	#warning Not implemented.
+}
+
+void setWifiSubnet(uint32_t mask)
+{
+	#warning Not implemented.
+}
+
+void setBlinkRate(uint8_t brate)
+{
+	#warning Not implemented.
+}
+
+void setSlideRate(uint8_t srate)
+{
+	#warning Not implemented.
+}
+
+void setPassword(const char *password)
+{
+	#warning Not implemented.
+}
--- Message.h
+++ Message.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <stdint.h>
 
-
 class Message {
 public:
 	enum Type {
@@ -16,16 +15,53 @@ public:
 	};
 
 	enum Error {
-		OK,
-		Failure,
-		MalformedPackage,
-		IncompatiblePackage,
-		IllegalWiFiConfig
+		OK = 0,
+		Failure = -1,
+		MalformedPackage = -2,
+		IncompatiblePackage = -3,
+		IllegalWiFiConfig = -4
 	};
 
-private:
-	Type type;
-	uint8_t size;
-	char content[256];
-};
+	Message(Type type);
+	Message(const void *raw);
 
+	// Getters
+	const char *text() const;
+
+	const char *wifiSSID() const;
+	const char *wifiPassword() const;
+	uint32_t wifiIP() const;
+	uint32_t wifiSubnet() const;
+
+	uint8_t blinkRate() const;
+	uint8_t slideRate() const;
+
+	const char *password() const;
+
+	uint8_t size() const
+	{
+		return mSize;
+	};
+
+	Type type() const {
+		return mType;
+	};
+
+	// Setters
+	void setWifiSSID(const char *str);
+	void setWifiPassword(const char *password);
+	void setWifiIP(uint32_t ip);
+	void setWifiSubnet(uint32_t mask);
+
+	void setBlinkRate(uint8_t brate);
+	void setSlideRate(uint8_t srate);
+
+	void setPassword(const char *password);
+
+	void prepare();
+
+private:
+	Type mType;
+	uint8_t mSize;
+	char rawContent[256];
+};
